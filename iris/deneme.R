3475ac4b8e9d556750144d7dc66ca399034d8c68
Hasan Ali ÖZKAN <hasanaliozkan-dev@outlook.com>
2022-07-28 14:53:51 +0300

    All things done

---
+++ iris/deneme.R
@@ -0,0 +1,25 @@
+#result matrix
+for (i 1:length(df)) {
+    #temparray #one dimension
+    for (j 1:length(df)) {
+        #öklid hesapla 
+        #öklid(df[i],df[j])
+        #temparray  <- append et       
+    }
+    #result matrix append et temparray
+    #temparray sıfırla
+}
+#heatmap çizdirceksin 
+
+function(compare,df ){
+    #ilk başta parametrelerin sayısına bakcaksın 
+    #compare = other
+    #öklid mesafesini al sonrasında heatmapi çizdir
+    
+    #compare = self
+    #ilk sıradaki parametreyi alıp for döngüsüne sokcan. 
+    #ikinci sıradaki parametreyi al fora sok.
+    #result  <-  result1 -result2
+    #tekrar heatmap çizdirip görselleştireceksin. 
+
+}
